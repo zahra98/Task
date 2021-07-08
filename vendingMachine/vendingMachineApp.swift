50a9daf17d121401c0fcc82ef8cdf3286434e652
zahra98 <zahraabuzahra4@gmail.com>
2021-07-08 10:56:03 +0300

    Initial Commit

---
+++ vendingMachine/vendingMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  vendingMachineApp.swift
+//  vendingMachine
+//
+//  Created by mac on 08/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct vendingMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
